5ef7d04ff9bd9b8d3006324e27087a55e84d72c0
Raul Pereira <raul.pereira@gmail.com>
2017-05-06 00:00:40 -0300

    class 8

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,13 @@
 class ProductsController < ApplicationController
 
-    def index
-        @products_by_name = Product.all.order(:name).limit 5
-        @products_by_price = Product.all.order(:price).limit 2
-    end
+  def index
+    @products_by_name = Product.all.order(:name).limit 5
+    @products_by_price = Product.all.order(:price).limit 2
+  end
+
+  def create
+    values = params.require(:product).permit :name, :description, :amount, :price
+    product = Product.create values
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
   root "products#index"
+  get "/products/new" => "products#new"
+  post "/products" => "products#create"
+
 end
